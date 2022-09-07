46dcf53c410ed25cbf81040e2cbabe4410668c06
Saba Mchedlishvili <sabamchedlishvili17@gmail.com>
2022-09-08 00:02:00 +0400

    project rearrangement

---
+++ src/app/features/Dashboard/components/sidebar/sidebar.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {LoginService} from "../../../login/services/login.service";
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SidebarComponent implements OnInit {
+
+  constructor(public loginService: LoginService) { }
+
+  ngOnInit(): void {
+  }
+}
